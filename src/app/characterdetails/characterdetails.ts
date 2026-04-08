ab672ec25156563d705dd7f5aaac0333790227ab
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-08 15:50:00 -0400

    Updated CharacterDetails

--- src/app/characterdetails/characterdetails.ts
+++ src/app/characterdetails/characterdetails.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharacterService } from '../shared/character-service';
+import { Character } from '../models/character';
 
 @Component({
   selector: 'app-characterdetails',
-  imports: [],
   templateUrl: './characterdetails.html',
-  styleUrl: './characterdetails.css',
+  styleUrls: ['./characterdetails.css'],
 })
-export class Characterdetails {}
+export class Characterdetails {
+
+  private route = inject(ActivatedRoute);
+  private service = inject(CharacterService);
+
+  character: Character | null = null;
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.service.getCharacterById(id)
+        .subscribe({
+          next: (data: Character) => {
+            this.character = data; // API returns a single Character
+          },
+          error: (err) => console.error('Error fetching character details', err)
+        });
+    }
+  }
+}
