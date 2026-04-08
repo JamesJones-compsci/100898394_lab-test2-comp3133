3e422024bc803799870bf0d29962869066d2e08e
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-08 16:33:50 -0400

    Updating home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,28 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.html', // correct
+  styleUrls: ['./app.css']   // <-- plural, Angular expects this
 })
 export class App {
+  // Title signal
   protected readonly title = signal('100898394-lab-test2-comp3133');
+
+  // Example character object (for testing)
+  character = {
+    name: 'Harry Potter',
+    species: 'Human',
+    house: 'Gryffindor',
+    wizard: true,
+    ancestry: 'Half-blood',
+    actor: 'Daniel Radcliffe',
+    image: 'https://hp-api.onrender.com/images/harry.jpg',
+    wand: {
+      wood: 'Holly',
+      core: 'Phoenix Feather',
+      length: 11
+    }
+  };
 }
