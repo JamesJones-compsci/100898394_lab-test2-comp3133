511bbdafd1a3eeb5cac0ee5fc8e9b5e2d0b93770
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-08 15:48:19 -0400

    Updated CharacterList

--- src/app/characterlist/characterlist.ts
+++ src/app/characterlist/characterlist.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CharacterService } from '../shared/character-service';
+import { Router } from '@angular/router';
+import { Character } from '../models/character';
 
 @Component({
   selector: 'app-characterlist',
-  imports: [],
   templateUrl: './characterlist.html',
-  styleUrl: './characterlist.css',
+  styleUrls: ['./characterlist.css'],  // correct array format
 })
-export class Characterlist {}
+export class Characterlist {
+
+  private service = inject(CharacterService);
+  private router = inject(Router);
+
+  characters: Character[] = [];
+  selectedHouse: string = "";
+
+  ngOnInit() {
+    this.loadCharacters();
+  }
+
+  loadCharacters() {
+    this.service.getCharacters().subscribe({
+      next: (data: Character[]) => {
+        this.characters = data;
+      },
+      error: (err) => console.error('Error fetching characters', err)
+    });
+  }
+
+  filterByHouse() {
+    if (!this.selectedHouse) {
+      this.loadCharacters();
+    } else {
+      this.service.getCharactersByHouse(this.selectedHouse)
+        .subscribe({
+          next: (data: Character[]) => this.characters = data,
+          error: (err) => console.error('Error filtering by house', err)
+        });
+    }
+  }
+
+  viewDetails(character: Character) {
+    this.router.navigate(['/character', character.id]);
+  }
+}
