1f78f2a3f1caa0a4a7341f5a4dbe91f4b2b4a5c9
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-08 16:59:58 -0400

    Continuing to work on routing issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Characterlist } from './characterlist/characterlist';
+import { Characterdetails } from './characterdetails/characterdetails';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Characterlist },
+  { path: 'character/:id', component: Characterdetails }
+];
