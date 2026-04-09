9ccd3691b9d7b6b2b4e2170fe5d34dae884d5fac
James Jones <jjsfitnesstoronto@gmail.com>
2026-04-08 21:33:49 -0400

    Ready for re-deployment

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+// src/polyfills.ts
+
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add global polyfills to this file, or import other files as needed.
+ */
+
+// Example: import 'zone.js'; is automatically included for Angular apps
+import 'zone.js';
